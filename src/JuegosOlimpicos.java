3fee15501ef94f7451e740a941666b8fdcc830a5
ericastro0615 <ericastro0615@gmail.com>
2024-09-01 21:10:39 -0300

    resubiendo

--- src/JuegosOlimpicos.java
+++ src/JuegosOlimpicos.java
@@ -1,2 +1,166 @@
-package PACKAGE_NAME;public class JuegosOlimpicos {
+/*TP 2.2 Ordenamiento y búsqueda
+        Cree una clase JJOO que contenga la siguiente información:
+        País, Cantidad de medallas de Bronce, Cantidad de medallas de plata, Cantidad de medallas de oro,
+        lugar del ranking.
+        Defina un comparador que considere lo siguiente los países se ordenan por cantidad de medallas de
+        oro, si 2 países tienen la misma cantidad de oros se comparan las platas, si también tienen la misma
+        cantidad de platas se comparan por bronces, en caso de coincidir, se colocan en orden alfabético.
+        Cree los métodos cargar y mostrar.
+        Luego agregue 2 de los métodos estudiados para ordenamiento a su elección.
+        Escriba un menú. Se espera que muestre los elementos en el orden ingresado y luego de ordenarlos, el
+        usuario debe poder elegir el método.
+        También tiene que mostrar el tiempo que insume el ordenamiento.*/
+
+import java.util.Arrays;
+
+public class JuegosOlimpicos implements Comparable<JuegosOlimpicos> {
+    private String pais;
+    private Integer medallasOro;
+    private Integer medallasPlata;
+    private Integer medallasBronce;
+    private Integer lugarRaking;
+
+    public JuegosOlimpicos(String pais, Integer medallasOro, Integer medallasPlata, Integer medallasBronce, Integer lugarRaking) {
+        this.pais = pais;
+        this.medallasOro = medallasOro;
+        this.medallasPlata = medallasPlata;
+        this.medallasBronce = medallasBronce;
+        this.lugarRaking = lugarRaking;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public Integer getMedallasOro() {
+        return medallasOro;
+    }
+
+    public void setMedallasOro(Integer medallasOro) {
+        this.medallasOro = medallasOro;
+    }
+
+    public Integer getMedallasPlata() {
+        return medallasPlata;
+    }
+
+    public void setMedallasPlata(Integer medallasPlata) {
+        this.medallasPlata = medallasPlata;
+    }
+
+    public Integer getMedallasBronce() {
+        return medallasBronce;
+    }
+
+    public void setMedallasBronce(Integer medallasBronce) {
+        this.medallasBronce = medallasBronce;
+    }
+
+    public Integer getLugarRaking() {
+        return lugarRaking;
+    }
+
+    public void setLugarRaking(Integer lugarRaking) {
+        this.lugarRaking = lugarRaking;
+    }
+
+    @Override
+    public String toString() {
+        return "  * " +
+                "pais '" + pais +
+                ", medallas de Oro= " + medallasOro +
+                ", medallas de Plata= " + medallasPlata +
+                ", medallas de Bronce= " + medallasBronce +
+                ", lugar en el Raking=" + lugarRaking +".";
+    }
+
+    // usar CompareTo y acceder a la opc cuales atributos se usaran p/ordenar
+    @Override
+    public int compareTo(JuegosOlimpicos juegoQueSeComparara) {
+        int comparacionOro = medallasOro.compareTo(juegoQueSeComparara.getMedallasOro());
+        if (comparacionOro != 0) {
+            return -comparacionOro; // Negativo para ordenar de mayor a menor
+        }
+
+        int comparacionPlata = medallasPlata.compareTo(juegoQueSeComparara.getMedallasPlata());
+        if (comparacionPlata != 0) {
+            return -comparacionPlata;
+        }
+
+        int comparacionBronce = medallasBronce.compareTo(juegoQueSeComparara.getMedallasBronce());
+        if (comparacionBronce != 0) {
+            return -comparacionBronce;
+        }
+
+        return pais.compareToIgnoreCase(juegoQueSeComparara.getPais());
+    }
+
+    public static JuegosOlimpicos cargar(String pais, Integer oro, Integer plata, Integer bronce, Integer ranking) {
+        return new JuegosOlimpicos(pais, oro, plata, bronce, ranking);
+    }
+
+
+    public static void bubbleSort(JuegosOlimpicos[] array) {
+        int len = array.length;
+        boolean cambiarPositionDeOrden;
+        for (int i = 0; i < len - 1; i++) {
+            cambiarPositionDeOrden = false;
+            for (int j = 0; j < len - 1; j++) {
+                // se compara obj en array. En compareTo se considera la posicion
+                // dentro del array y se compara con num 0 igual, - menor, + ma
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    JuegosOlimpicos temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    cambiarPositionDeOrden = true;
+                }
+            }
+            if (!cambiarPositionDeOrden) {
+                break;
+            }
+        }
+    }
+
+    public static void insertionSort(JuegosOlimpicos[] array) {
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            JuegosOlimpicos varTemporal = array[i];
+            int j = i - 1;
+            // Mover los elementos de datos[0...i-1] que son mayores que varTemporal
+            // una posición adelante de su posición actual
+            while (j >= 0 && array[j].compareTo(varTemporal)>0 ){
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = varTemporal;
+        }
+    }
+    public static void shellSort(JuegosOlimpicos[] array) {
+        int len = array.length;
+
+        // intervalo inicial
+        for (int intervalo = len / 2; intervalo > 0; intervalo /= 2) {
+            for (int i = intervalo; i < len; i++) {
+                JuegosOlimpicos varTemporal = array[i];
+                int j = i;
+
+                // Se compara obj en array usando compareTo, determinado por el intervalo
+                while (j >= intervalo && array[j - intervalo].compareTo(varTemporal) > 0) {
+                    array[j] = array[j - intervalo];
+                    j -= intervalo;
+                }
+                array[j] = varTemporal;
+            }
+        }
+    }
+
+    public static void muestraArrayOrdenado (JuegosOlimpicos[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,124 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Alt+F5 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing F9.
-            System.out.println("i = " + i);
+    public static void mostrarPaises(JuegosOlimpicos[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + "\n");
         }
+    }
+
+    public static void main(String[] args) {
+        JuegosOlimpicos[] paisesJugadores = new JuegosOlimpicos[33];
+        carga(paisesJugadores);
+
+        Scanner sc = new Scanner(System.in);
+        menu(sc, paisesJugadores);
+    }
+
+    public static void menu(Scanner scanner, JuegosOlimpicos[] paisesJugadores) {
+        int eleccion = 0;
+
+        while (eleccion != 4) {
+            System.out.println("\nSeleccione el metodo de ordenamiento:");
+            System.out.println("1. Bubble Sort");
+            System.out.println("2. Insertion Sort");
+            System.out.println("3. Shell Sort");
+            System.out.println("4. Salir");
+            eleccion = scanner.nextInt();
+
+            long startTime = System.nanoTime();
+
+            switch (eleccion) {
+                case 1:
+                    System.out.println("Estado original del array pre bubble sort:");
+                    mostrarPaises(paisesJugadores);
+
+                    JuegosOlimpicos.bubbleSort(paisesJugadores);
+                    System.out.println("Metodo bubble");
+
+                    System.out.println("Estado del array post metodo de bubble sort:");
+                    mostrarPaises(paisesJugadores);
+                    break;
+
+                case 2:
+                    System.out.println("Estado original del array pre insertion sort:");
+                    mostrarPaises(paisesJugadores);
+
+                    JuegosOlimpicos.insertionSort(paisesJugadores);
+                    System.out.println("Metodo insertionSort");
+
+                    System.out.println("Estado del array post metodo de insertion sort:");
+                    mostrarPaises(paisesJugadores);
+                    break;
+
+                case 3:
+                    System.out.println("Estado original del array pre shell sort:");
+                    mostrarPaises(paisesJugadores);
+
+                    JuegosOlimpicos.shellSort(paisesJugadores);
+                    System.out.println("Metodo shell Sort");
+
+                    System.out.println("Estado del array post metodo de shell sort:");
+                    mostrarPaises(paisesJugadores);
+                    break;
+
+                case 4:
+                    System.out.println("Adios!");
+                    break;
+
+                default:
+                    System.out.println("Error, reintente nuevamente.");
+                    continue;
+            }
+
+            if (eleccion != 4) {
+                long endTime = System.nanoTime();
+                long duration = (endTime - startTime) / 1000000;
+
+            //    System.out.println("\nPaises ordenados:");
+             //   mostrarPaises(paisesJugadores);
+                System.out.println("Tiempo de ejecucion para el ordenamiento: " + duration + " ms");
+
+                // Restaurar el array a su estado original post ordenamiento
+                carga(paisesJugadores);
+            }
+        }
+    }
+
+    public static void carga(JuegosOlimpicos[] paisesJugadores) {
+        paisesJugadores[0] = new JuegosOlimpicos("Bolivia", 2, 5, 7, 4);
+        paisesJugadores[1] = new JuegosOlimpicos("Argentina", 4, 5, 7, 1);
+        paisesJugadores[2] = new JuegosOlimpicos("Brasil", 2, 5, 7, 4);
+        paisesJugadores[3] = new JuegosOlimpicos("Chile", 3, 6, 8, 2);
+        paisesJugadores[4] = new JuegosOlimpicos("Ecuador", 1, 4, 6, 3);
+        paisesJugadores[5] = new JuegosOlimpicos("Perú", 5, 6, 3, 5);
+        paisesJugadores[6] = new JuegosOlimpicos("Uruguay", 2, 3, 4, 6);
+        paisesJugadores[7] = new JuegosOlimpicos("Colombia", 4, 2, 5, 7);
+        paisesJugadores[8] = new JuegosOlimpicos("Paraguay", 1, 1, 2, 8);
+        paisesJugadores[9] = new JuegosOlimpicos("Venezuela", 3, 5, 6, 9);
+        paisesJugadores[10] = new JuegosOlimpicos("México", 6, 8, 5, 4);
+        paisesJugadores[11] = new JuegosOlimpicos("Estados Unidos", 9, 7, 8, 3);
+        paisesJugadores[12] = new JuegosOlimpicos("Canadá", 7, 6, 4, 5);
+        paisesJugadores[13] = new JuegosOlimpicos("España", 4, 9, 3, 7);
+        paisesJugadores[14] = new JuegosOlimpicos("Francia", 8, 4, 6, 2);
+        paisesJugadores[15] = new JuegosOlimpicos("Italia", 5, 3, 9, 6);
+        paisesJugadores[16] = new JuegosOlimpicos("Alemania", 9, 8, 7, 4);
+        paisesJugadores[17] = new JuegosOlimpicos("Reino Unido", 6, 7, 5, 8);
+        paisesJugadores[18] = new JuegosOlimpicos("Australia", 7, 4, 6, 9);
+        paisesJugadores[19] = new JuegosOlimpicos("Japón", 5, 6, 4, 7);
+        paisesJugadores[20] = new JuegosOlimpicos("China", 8, 7, 9, 5);
+        paisesJugadores[21] = new JuegosOlimpicos("India", 4, 5, 6, 8);
+        paisesJugadores[22] = new JuegosOlimpicos("Sudáfrica", 6, 7, 8, 4);
+        paisesJugadores[23] = new JuegosOlimpicos("Noruega", 3, 6, 4, 7);
+        paisesJugadores[24] = new JuegosOlimpicos("Suecia", 5, 8, 6, 3);
+        paisesJugadores[25] = new JuegosOlimpicos("Portugal", 4, 6, 5, 3);
+        paisesJugadores[26] = new JuegosOlimpicos("Grecia", 7, 4, 2, 6);
+        paisesJugadores[27] = new JuegosOlimpicos("Turquía", 6, 5, 8, 4);
+        paisesJugadores[28] = new JuegosOlimpicos("Irán", 3, 7, 6, 5);
+        paisesJugadores[29] = new JuegosOlimpicos("Arabia Saudita", 5, 3, 4, 6);
+        paisesJugadores[30] = new JuegosOlimpicos("Emiratos Árabes Unidos", 8, 2, 7, 4);
+        paisesJugadores[31] = new JuegosOlimpicos("Israel", 4, 6, 3, 7);
+        paisesJugadores[32] = new JuegosOlimpicos("Pakistán", 2, 5, 8, 4);
     }
 }
